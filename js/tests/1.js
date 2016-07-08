992b5c7a8d09f2c8fc4377d7d024e7543567a170
james-walker-develop <james.walker.misc@gmail.com>
2016-07-08 14:10:03 +1000

    testing

--- js/tests/1.js
+++ js/tests/1.js
@@ -1,9 +1,10 @@
+var test = document.getElementById('glWrapper');
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, test.innerWidth / test.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( test.innerWidth,test.innerHeight);
+test.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
